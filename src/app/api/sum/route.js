8041882d6eec08e74a2f2b52597a5ecd700355e7
Aravind <aravindgotojapan@gmail.com>
2023-10-31 16:53:46 +0530

    update for group summary

--- src/app/api/sum/route.js
+++ src/app/api/sum/route.js
@@ -1,32 +1,31 @@
 import getPSConnection from '@/lib/planetscaledb';
 
-// Function to fetch all groups details or a certain group's details from the database
 async function getGroupsData(connection, groupName) {
   const query = groupName
     ? `
     SELECT 
       \`Device ID\`, \`company\`, \`Group\`, Type, CoD, Year,\`project\`,
-      MAX(CASE WHEN Month = 'January' THEN Sold ELSE NULL END) AS January,
-      MAX(CASE WHEN Month = 'February' THEN Sold ELSE NULL END) AS February,
-      MAX(CASE WHEN Month = 'March' THEN Sold ELSE NULL END) AS March,
-      MAX(CASE WHEN Month = 'April' THEN Sold ELSE NULL END) AS April,
-      MAX(CASE WHEN Month = 'May' THEN Sold ELSE NULL END) AS May,
-      MAX(CASE WHEN Month = 'June' THEN Sold ELSE NULL END) AS June,
-      MAX(CASE WHEN Month = 'July' THEN Sold ELSE NULL END) AS July,
-      MAX(CASE WHEN Month = 'August' THEN Sold ELSE NULL END) AS August,
-      MAX(CASE WHEN Month = 'September' THEN Sold ELSE NULL END) AS September,
-      MAX(CASE WHEN Month = 'October' THEN Sold ELSE NULL END) AS October,
-      MAX(CASE WHEN Month = 'November' THEN Sold ELSE NULL END) AS November,
-      MAX(CASE WHEN Month = 'December' THEN Sold ELSE NULL END) AS December
+      COALESCE(MAX(CASE WHEN Month = 'January' THEN Sold ELSE NULL END), 0) AS January,
+      COALESCE(MAX(CASE WHEN Month = 'February' THEN Sold ELSE NULL END), 0) AS February,
+      COALESCE(MAX(CASE WHEN Month = 'March' THEN Sold ELSE NULL END), 0) AS March,
+      COALESCE(MAX(CASE WHEN Month = 'April' THEN Sold ELSE NULL END), 0) AS April,
+      COALESCE(MAX(CASE WHEN Month = 'May' THEN Sold ELSE NULL END), 0) AS May,
+      COALESCE(MAX(CASE WHEN Month = 'June' THEN Sold ELSE NULL END), 0) AS June,
+      COALESCE(MAX(CASE WHEN Month = 'July' THEN Sold ELSE NULL END), 0) AS July,
+      COALESCE(MAX(CASE WHEN Month = 'August' THEN Sold ELSE NULL END), 0) AS August,
+      COALESCE(MAX(CASE WHEN Month = 'September' THEN Sold ELSE NULL END), 0) AS September,
+      COALESCE(MAX(CASE WHEN Month = 'October' THEN Sold ELSE NULL END), 0) AS October,
+      COALESCE(MAX(CASE WHEN Month = 'November' THEN Sold ELSE NULL END), 0) AS November,
+      COALESCE(MAX(CASE WHEN Month = 'December' THEN Sold ELSE NULL END), 0) AS December
     FROM (
       SELECT 
         \`Device ID\`, \`company\`, \`Group\`, Type, CoD, Year, Month,\`project\`,
         CASE
-          WHEN Actual = Sold AND Actual != 0 THEN 'Sold'
-          WHEN Actual != 0 AND Actual = Actual_used THEN 'Reserved'
-          WHEN Actual = 0 AND Estimated = Estimated_used THEN 'Reserved'
-          WHEN Actual != 0 THEN Actual - Actual_used
-          ELSE Estimated - Estimated_used
+          WHEN COALESCE(\`Actual\`, 0) = COALESCE(\`Issued\`, 0) AND COALESCE(\`Actual\`, 0) != 0 THEN 'Sold'
+          WHEN COALESCE(\`Actual\`, 0) != 0 AND COALESCE(\`Actual\`, 0) = COALESCE(\`Actual_used\`, 0 ) THEN 'Reserved'
+          WHEN COALESCE(\`Actual\`, 0) = 0 AND \`Estimated\` = \`Estimated_used\` THEN 'Reserved'
+          WHEN COALESCE(\`Actual\`, 0) != 0 THEN COALESCE(\`Actual\`, 0) - COALESCE(\`Actual_used\`, 0)
+          ELSE COALESCE(\`Estimated\`, 0) - COALESCE(\`Estimated_used\`, 0)
         END AS Sold
       FROM \`inventory2\`
       WHERE \`Group\` = ?
@@ -40,9 +39,7 @@ async function getGroupsData(connection, groupName) {
   return rows;
 }
 
-// Define an asynchronous function to handle GET requests
 export async function POST(request) {
-  // Get a connection to the PlanetscaleDB database
   const connection = await getPSConnection();
 
   const requestBody = await request.json();
@@ -50,10 +47,8 @@ export async function POST(request) {
 
   const groupsData = await getGroupsData(connection, groupName);
 
-  // Close the database connection
   connection.end();
 
-  // Return the data as a JSON object
   return new Response(JSON.stringify(groupsData), {
     headers: { 'Content-Type': 'application/json' },
   });
