4b0b58068380989f7d1af57a50c14e823a6ac2e8
Aravind <aravindgotojapan@gmail.com>
2023-11-14 12:36:57 +0530

    filter update for beta transactions

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
         <ListItem text={'Transactions'} navLink={'/dash/txn'} Icon={BanknotesIcon}/>
         <ListItem text={'Device Summary'} navLink={'/dash/sum'} Icon={DocumentMagnifyingGlassIcon}/>
         <ListItem text={'Group Summary'} navLink={'/dash/group'} Icon={SparklesIcon}/> 
-        <ListItem text={'transaction Beta'} navLink={'/dash/txnbeta'} Icon={SparklesIcon}/>                  
+        <ListItem text={'Merge transactions'} navLink={'/dash/txnbeta'} Icon={SparklesIcon}/>                  
     </div>
     <div className='flex items-center mb-4 mt-4 ml-2'>
         <UserButton afterSignOutUrl='/' />
