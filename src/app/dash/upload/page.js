9ca0b84c05a94dc60a8ac901faa77874fed22ad2
Aravind <aravindgotojapan@gmail.com>
2023-12-08 21:09:59 +0530

    seller data upload button name change

--- src/app/dash/upload/page.js
+++ src/app/dash/upload/page.js
@@ -39,7 +39,7 @@ const UploadButtons = () => {
       </Link>
       <Link href="/dash/upload/seller">
         <button className="cursor-pointer transform transition duration-500 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:scale-105 active:scale-95">
-          sellers data
+          sellers data upload
         </button>
       </Link>
       <button
